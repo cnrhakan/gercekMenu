83c3fa502e238b260b7ebcfcbbc41f941025c7cc
Hakan Çınar <80056244+cnrhakan@users.noreply.github.com>
2025-08-07 00:34:36 +0300

    Delete src directory

--- src/components/ValidateMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ValidateMessage({ message }) {
-  return (
-    <div className="text-red-500 text-sm pl-1 font-semibold">{message}</div>
-  );
-}
-
-export default ValidateMessage;
--- src/components/fetchAllProfiles.jsx
+++
@@ -1,9 +0,0 @@
-import supabase from "../supabaseClient";
-import { useQuery } from "@tanstack/react-query";
-
-export const fetchAllProfiles = async () => {
-  const { data, error } = await supabase.from("profiles").select("*");
-
-  if (error) throw new Error("Kullanıcı bilgisi alınamadı");
-  return data;
-};
